e8f04a9803faff6a429f7a944b051caa7ca11d83
Clément Martinez <clementmartinezdev@gmail.com>
2017-03-08 20:39:58 +0100

    Add skeleton

---
+++ src/term_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "termbox_graphics.h"
+
+int main(int argc, char **argv) {
+        termbox_graphics_t *graphics = termbox_graphics_init();
+        if (graphics == NULL) {
+                fprintf(stderr, "Could not initialize termbox");
+                return 1;
+        }
+
+        termbox_graphics_quit(graphics);
+        return 0;
+}
---
+++ src/termbox_graphics.h
@@ -0,0 +1,7 @@
+#include <termbox.h>
+
+typedef struct {} termbox_graphics_t;
+
+termbox_graphics_t* termbox_graphics_init();
+void termbox_graphics_draw(termbox_graphics_t *tg);
+void termbox_graphics_quit(termbox_graphics_t *tg);
