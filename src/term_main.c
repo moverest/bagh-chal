fc5a01722aef782797671a75b3416771aed6db6c
Clément Martinez <clementmartinezdev@gmail.com>
2017-03-08 21:31:10 +0100

    Run format.sh

--- src/term_main.c
+++ src/term_main.c
@@ -3,12 +3,13 @@
 #include "termbox_graphics.h"
 
 int main(int argc, char **argv) {
-        termbox_graphics_t *graphics = termbox_graphics_init();
-        if (graphics == NULL) {
-                fprintf(stderr, "Could not initialize termbox");
-                return 1;
-        }
+    termbox_graphics_t *graphics = termbox_graphics_init();
 
-        termbox_graphics_quit(graphics);
-        return 0;
+    if (graphics == NULL) {
+        fprintf(stderr, "Could not initialize termbox");
+        return 1;
+    }
+
+    termbox_graphics_quit(graphics);
+    return 0;
 }
--- src/termbox_graphics.c
+++ src/termbox_graphics.c
@@ -2,24 +2,26 @@
 #include <stdlib.h>
 #include "termbox_graphics.h"
 
-termbox_graphics_t* termbox_graphics_init() {
-        termbox_graphics_t *tg = malloc(sizeof(termbox_graphics_t));
-        if (tg == NULL) {
-                return NULL;
-        }
+termbox_graphics_t *termbox_graphics_init() {
+    termbox_graphics_t *tg = malloc(sizeof(termbox_graphics_t));
 
-        if (tb_init()) {
-                return NULL;
-        }
+    if (tg == NULL) {
+        return NULL;
+    }
 
-        return tg;
+    if (tb_init()) {
+        return NULL;
+    }
+
+    return tg;
 }
+
 
 void termbox_graphics_draw(termbox_graphics_t *tg) {
-
 }
 
+
 void termbox_graphics_quit(termbox_graphics_t *tg) {
-        tb_shutdown();
-        free(tg);
+    tb_shutdown();
+    free(tg);
 }
