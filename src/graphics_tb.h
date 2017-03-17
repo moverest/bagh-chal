79ecdd107f7531d36f69d6e7089b4a685a370312
Clément Martinez <clementmartinezdev@gmail.com>
2017-03-17 23:48:08 +0100

    Add an explanation for graphics_*_draw prototype

--- src/graphics_tb.h
+++ src/graphics_tb.h
@@ -8,13 +8,17 @@
 
 typedef struct {}   graphics_tb_t;
 
-// graphics_tb initialize the graphic module.
+// graphics_tb initializes the graphic module.
 graphics_tb_t *graphics_tb_init();
 
 // graphics_tb_draw draws the given state on the screen.
+//
+// We do not use `graphics_tb_t *tg` but `void *context` because this function
+// is used as a callback and the caller shouldn't have to know about
+// graphics_tb. This way, this module is pluggable.
 void graphics_tb_draw(void *context, state_to_draw_t *state);
 
-// graphics_tb_quit terminate the graphic module.
+// graphics_tb_quit terminates the graphic module.
 void graphics_tb_quit(graphics_tb_t *tg);
 
 
