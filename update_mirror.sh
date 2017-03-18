95d0b086a22a321ee6cedcd8cf94eb2086b5fc83
Clément Martinez <clementmartinezdev@gmail.com>
2017-03-18 10:22:44 +0100

    Improve the mirror sync tool
    It now adds the remote to the current git repository instead of cloning
    the repository and then pushing the changes.

--- update_mirror.sh
+++ update_mirror.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-git clone 'git@github.com:/moverest/cshellgame'
-cd cshellgame || exit
-git remote add forge 'https://forge.telecomnancy.univ-lorraine.fr/git/martine96u_c'
-git push --mirror forge
-cd ..
-rm -rf cshellgame
+function init {
+    git remote add mirror 'https://forge.telecomnancy.univ-lorraine.fr/git/martine96u_c'
+}
+
+function sync {
+    git push --mirror mirror
+}
+
+function is_init {
+    git remote | grep mirror > /dev/null
+}
+
+case "$1" in
+    init )
+        init
+        ;;
+
+    sync )
+        sync
+        ;;
+
+    is_init )
+        is_init && echo initialised || echo not initialised && false
+        ;;
+
+    * )
+        is_init || init
+        sync
+        ;;
+
+esac
