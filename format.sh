02fd4a47d2106b8c700b8f8330a1b6a651ed4f9b
Clément Martinez <clementmartinezdev@gmail.com>
2017-03-19 22:22:47 +0100

    Improve format.sh
    Enable to format a single file instead of all of them
    Add usage help

--- format.sh
+++ format.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-for file in $(find src -name '*.c' -o -name '*.h')
-do
-    uncrustify -c format.cfg --no-backup -l c $file
-done
+file_path=src/
+
+function usage {
+    echo 'Format given file(s)'
+    echo "    $0 <file name> [... <file name>]"
+    echo
+    echo "Format every files in $file_path"
+    echo "    $0"
+    echo
+    echo 'Show help'
+    echo "    $0 --help"
+    echo "    $0 -h"
+}
+
+function format_all {
+    for file in $(find "$file_path" -name '*.c' -o -name '*.h')
+    do
+        format_file "$file"
+    done
+}
+
+function format_file {
+    uncrustify -c format.cfg --no-backup -l c "$1"
+}
+
+if [ -z "$1" ]
+then
+    format_all
+elif [ "$1" = '--help' ] || [ "$1" = '-h' ]
+then
+    usage
+else
+    for file in "$@"
+    do
+        format_file "$file"
+    done
+fi
